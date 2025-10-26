c1ba11b349b5bfdc610ecbf26234ddbdd100eefa
seonghoon <seonghoon315@naver.com>
2025-10-26 18:20:46 +0900

    feat(domain): 자동차 경주 게임 핵심 로직 구현
    - setupCars(String[] names): 자동차 이름 배열을 받아 게임 상태(이름, 이동 거리)를 초기화하는 기능
    - playRound(): 한 라운드를 진행하며, 각 자동차에 대해 무작위 값(0~9)을 생성하고 4 이상일 경우 전진('-' 추가)시키는 기능
    - getWinners(): 모든 라운드 종료 후, 가장 많이 전진한 자동차를 최종 우승자로 판별하는 기능

---
+++ src/main/java/domain/GameMachine.java
@@ -0,0 +1,58 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GameMachine {
+    /*
+    1. 각 자동차는 0~9 사이의 무작위 수를 생성한다.
+    2. 무작위 값이 4 이상일 경우 전진(`-` 추가), 그렇지 않으면 멈춘다.
+    3. 모든 시도 횟수만큼 경주를 반복 수행한다.
+    4. 매 시도마다 각 자동차의 이름과 현재 전진 상태를 출력한다.
+     */
+
+    private String[] carNames;
+    private String[] moveCounts;
+
+    public void setupCars(String[] names) {
+        this.carNames = names;
+        this.moveCounts = new String[names.length];
+
+        Arrays.fill(this.moveCounts, "");
+    }
+
+    public void playRound() {
+        if (carNames == null) {
+            return;
+        }
+
+        for (int i = 0; i < carNames.length; i++) {
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+
+            if (randomNumber >= 4) {
+                moveCounts[i] += "-";
+            }
+        }
+    }
+
+    public List<String> getWinners() {
+        int maxScore = 0;
+        List<String> winners = new ArrayList<>();
+
+        for (String move : this.moveCounts) {
+            if (move.length() > maxScore) {
+                maxScore = move.length();
+            }
+        }
+
+        for (int i = 0; i < this.carNames.length; i++) {
+            if (this.moveCounts[i].length() == maxScore) {
+                winners.add(this.carNames[i]);
+            }
+        }
+
+        return winners;
+    }
+}
--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import domain.GameMachine;
 import validate.Validator;
 import view.InputView;
 
@@ -7,10 +8,17 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         Validator validator = new Validator();
+        GameMachine gameMachine = new GameMachine();
 
         String[] players = inputView.setPlayers();
         validator.validatePlayersName(players);
         int playTimes = inputView.setPlayTimes();
         validator.validatePlayTimes(playTimes);
+
+        gameMachine.setupCars(players);
+        for (int i = 0; i < playTimes; i++) {
+            gameMachine.playRound();
+        }
+        gameMachine.getWinners();
     }
 }
