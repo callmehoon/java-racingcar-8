23798abf2d2a101323ea60913c26f2c42324e06c
seonghoon <seonghoon315@naver.com>
2025-10-26 16:40:06 +0900

    feat(view): 입력 처리 기능 구현
    - `Console.readLine()`을 이용하여 사용자 입력 처리

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,24 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    /*
+    1. 경주할 자동차 이름을 입력받는다. (쉼표(,) 기준으로 구분)
+    2. 시도할 횟수를 입력받는다.
+    3. 사용자 입력은 Console.readLine()을 이용한다.
+     */
+
+    public String[] setPlayers() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String players = Console.readLine();
+
+        return players.split("[,]");
+    }
+
+    public int setPlayTimes() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+
+        return Integer.parseInt(Console.readLine());
+    }
+}
